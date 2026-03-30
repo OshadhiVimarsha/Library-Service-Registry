d82ba0f9f0296754fa4b53f7dbf8d83de59c5f9b
oshivimarsha <oshadivimarsha@gmail.com>
2026-03-30 08:24:48 +0100

    Update Changes

--- src/main/java/lk/ijse/eca/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/lk/ijse/eca/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package lk.ijse.serviceregistry;
+package lk.ijse.eca.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
